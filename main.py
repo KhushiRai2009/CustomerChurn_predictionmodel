85fdaca959ea968c8da0b9ae94d442c2ccdf1d9f
KhushiRai2009 <khushirai6561@gmail.com>
2026-03-18 15:03:49 +0530

    done

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src.data_ingestion import data_ingestion
-
+from src.data_preprocessing import data_preprocessing
 
 def main():
 
     # step 1: Data Ingestion
     df=data_ingestion()
     print(df.shape)
+    X_train,X_test,y_train,y_test=data_preprocessing(df)
+    print(X_train,X_test,y_train,y_test)
 
 
 main()
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -22,6 +22,9 @@ def data_preprocessing(df):
 
     #1.Clean the unwanted columns from the dataset
     df=df.drop_duplicates()
+
+    # Encode target to 0 and 1
+    df["Churn"]=df["Churn"].apply(lambda x:1 if x=="Yes" else 0)
     
     #2.split the data into X and y
     X=df.drop(columns=["customerID","Churn"])
@@ -37,5 +40,35 @@ def data_preprocessing(df):
     categorical_col=X_train.select_dtypes(include="object").columns
 
     #5.using Pipeline for numerical and categorical columns
+    Numerical_Pipeline=Pipeline(steps=[
+        ("Imputer",SimpleImputer(strategy="median")),
+        ("Scaling",MinMaxScaler())
+    ])
 
+    Categorical_Pipeline=Pipeline(steps=[
+        ("Imputer",SimpleImputer(strategy="most_frequent")),
+        ("Encoder",OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    #6.use columns Transformer to fit our model
+    Preprocessor=ColumnTransformer(transformers=[
+        ("Numerical_pipe",Numerical_Pipeline,numerical_col),
+        ("Categorical_pipe",Categorical_Pipeline,categorical_col)
+    ])
+
+    X_train=Preprocessor.fit_transform(X_train)
+    X_test=Preprocessor.transform(X_test)
+
+    # Use SMOTE Technique
+    sm=SMOTE()
+
+    X_train,y_train=sm.fit_resample(X_train,y_train)
+
+    # Use PCA (Principal Component Analysis:Dimension Reductionality Technique)
+    pca=PCA()
+
+    X_train=pca.fit_transform(X_train)
+    X_test=pca.transform(X_test)
+
+    return X_train,X_test,y_train,y_test
 
